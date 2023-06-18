69b64a5e2d9c284ea73ca2499db767719ec827d7
flucium <14249224+flucium@users.noreply.github.com>
2023-06-19 02:06:44 +0900

    update ./src/symmetric/mod.rs
    implemented chacha20poly1305

--- src/symmetric/mod.rs
+++ src/symmetric/mod.rs
@@ -1,8 +1,67 @@
 mod chacha;
 
-use crate::{rand::generate, Error, ErrorKind, Result};
+use self::chacha::{ChaCha20Poly1305, XChaCha20Poly1305};
 
-use aead::{Aead, Payload};
+use crate::{
+    rand::generate,
+    size::{SIZE_U12, SIZE_U32},
+    Error, ErrorKind, Result,
+};
+
+use aead::{Aead, KeyInit, Payload};
+
+pub enum Symmetric<'a> {
+    ChaCha20Poly1305 {
+        key: Box<[u8; SIZE_U32]>,
+        aad: &'a [u8],
+        msg: &'a [u8],
+    },
+    XChaCha20Poly1305 {
+        key: Box<[u8; SIZE_U32]>,
+        aad: &'a [u8],
+        msg: &'a [u8],
+    },
+}
+
+impl Symmetric<'_> {
+    pub fn encrypt(self) -> Result<Vec<u8>> {
+        match self {
+            Self::ChaCha20Poly1305 { key, aad, msg } => aead_encrypt(
+                ChaCha20Poly1305::new_from_slice(&*key)
+                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+                SIZE_U12,
+                aad,
+                msg,
+            ),
+            Self::XChaCha20Poly1305 { key, aad, msg } => aead_encrypt(
+                XChaCha20Poly1305::new_from_slice(&*key)
+                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+                SIZE_U12,
+                aad,
+                msg,
+            ),
+        }
+    }
+
+    pub fn decrypt(self) -> Result<Vec<u8>> {
+        match self {
+            Self::ChaCha20Poly1305 { key, aad, msg } => aead_decrypt(
+                ChaCha20Poly1305::new_from_slice(&*key)
+                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+                SIZE_U12,
+                aad,
+                msg,
+            ),
+            Self::XChaCha20Poly1305 { key, aad, msg } => aead_decrypt(
+                XChaCha20Poly1305::new_from_slice(&*key)
+                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+                SIZE_U12,
+                aad,
+                msg,
+            ),
+        }
+    }
+}
 
 fn aead_decrypt(aead: impl Aead, nonce_size: usize, aad: &[u8], cipher: &[u8]) -> Result<Vec<u8>> {
     let len = cipher.len() - nonce_size;
