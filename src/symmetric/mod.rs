46a984031909cd753f7d26fe394402c4a67dccaf
flucium <14249224+flucium@users.noreply.github.com>
2023-06-18 15:48:55 +0900

    update ./src/symmetric/mod.rs
    implemented aead_encrypt(...)...{...} and aead_decrypt(...)...{...}

--- src/symmetric/mod.rs
+++ src/symmetric/mod.rs
@@ -1,1 +1,50 @@
 mod chacha;
+
+use crate::{rand::generate, Error, ErrorKind, Result};
+
+use aead::{Aead, Payload};
+
+fn aead_decrypt(aead: impl Aead, nonce_size: usize, aad: &[u8], cipher: &[u8]) -> Result<Vec<u8>> {
+    let len = cipher.len() - nonce_size;
+
+    let nonce = match cipher.get(len..) {
+        None => return Err(Error::new(ErrorKind::Todo, "".to_string())),
+        Some(nonce) => nonce,
+    };
+
+    let cipher = match cipher.get(..len) {
+        None => return Err(Error::new(ErrorKind::Todo, "".to_string())),
+        Some(cipher) => cipher,
+    };
+
+    let plain = aead
+        .decrypt(
+            nonce.into(),
+            Payload {
+                msg: cipher,
+                aad: aad,
+            },
+        )
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+
+    Ok(plain)
+}
+
+fn aead_encrypt(aead: impl Aead, nonce_size: usize, aad: &[u8], plain: &[u8]) -> Result<Vec<u8>> {
+    let r = generate();
+    let nonce = r.get(0..nonce_size).unwrap();
+
+    let mut cipher = aead
+        .encrypt(
+            nonce.into(),
+            Payload {
+                msg: plain,
+                aad: aad,
+            },
+        )
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+
+    cipher.append(&mut nonce.to_vec());
+
+    Ok(cipher)
+}
