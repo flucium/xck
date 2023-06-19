fe7bd5da3d6854f5a079b4c8702619a8434b1536
flucium <14249224+flucium@users.noreply.github.com>
2023-06-19 14:21:07 +0900

    update ./src/hash.rs
    added BLAKE3

--- src/hash.rs
+++ src/hash.rs
@@ -16,3 +16,26 @@ pub fn sha512(bytes: &[u8]) -> [u8; SIZE_U64] {
 pub fn sha512_256(bytes: &[u8]) -> [u8; SIZE_U32] {
     Sha512_256::digest(bytes).into()
 }
+
+/// BLAKE3
+pub fn blake3(bytes: &[u8]) -> [u8; SIZE_U32] {
+    blake3::hash(bytes).into()
+}
+
+/// BLAKE3 XOF(extend)
+pub fn blake3_xof(bytes: &[u8], output: &mut [u8]) {
+    blake3::Hasher::new()
+        .update(bytes)
+        .finalize_xof()
+        .fill(output);
+}
+
+/// BLAKE3 Key Derivation
+pub fn blake3_kdf(ikm: &[u8], context: &str) -> [u8; SIZE_U32] {
+    blake3::derive_key(context, ikm)
+}
+
+/// BLAKE3 MAC
+pub fn blake3_mac(key: &[u8; 32], msg: &[u8]) -> [u8; SIZE_U32] {
+    blake3::keyed_hash(key, msg).into()
+}
