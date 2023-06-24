8c4b0e2a3df58c3f02af63ef21ebcd1d0be6bc7f
flucium <14249224+flucium@users.noreply.github.com>
2023-06-24 11:16:01 +0900

    update ./src/lib.rs
    added Error

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+/// Result ...
+pub type Result<T> = core::result::Result<T, Error>;
+
+/// Error ...
+#[derive(Debug)]
+pub struct Error {
+    kind: ErrorKind,
+    message: String,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, message: String) -> Self {
+        Self {
+            kind: kind,
+            message: message,
+        }
+    }
+
+    pub fn kind(&self) -> &ErrorKind {
+        &self.kind
+    }
+
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+/// ErrorKind ...
+#[derive(Debug)]
+pub enum ErrorKind {
+    Todo,
+}
