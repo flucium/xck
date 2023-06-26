724109b116e8aa6449a9ec17de79417091ad8e0a
flucium <14249224+flucium@users.noreply.github.com>
2023-06-26 15:45:03 +0900

    update ./src/symmetric.rs

--- src/symmetric.rs
+++ src/symmetric.rs
@@ -5,82 +5,48 @@ use crate::{
 use aead::{Aead, KeyInit, Payload};
 use chacha20poly1305::{ChaCha20Poly1305, XChaCha20Poly1305};
 
-/// Symmetric ...
-pub enum Symmetric<'a> {
-    ChaCha20Poly1305 {
-        key: Box<[u8; SIZE_32]>,
-        nonce: Box<[u8; SIZE_12]>,
-        aad: &'a [u8],
-        msg: &'a [u8],
-    },
-    XChaCha20Poly1305 {
-        key: Box<[u8; SIZE_32]>,
-        nonce: Box<[u8; SIZE_24]>,
-        aad: &'a [u8],
-        msg: &'a [u8],
-    },
+pub fn chacha20poly1305_decrypt(
+    key: [u8; SIZE_32],
+    nonce: [u8; SIZE_12],
+    aad: &[u8],
+    plain: &[u8],
+) -> Result<Vec<u8>> {
+    let aead = ChaCha20Poly1305::new_from_slice(&key)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+    aead_encrypt(aead, &nonce, aad, plain)
 }
 
-impl Symmetric<'_> {
-    /// Encrypt ...
-    pub fn encrypt(self) -> Result<Vec<u8>> {
-        match self {
-            Self::ChaCha20Poly1305 {
-                key,
-                nonce,
-                aad,
-                msg,
-            } => aead_encrypt(
-                ChaCha20Poly1305::new_from_slice(&*key)
-                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
-                &*nonce,
-                aad,
-                msg,
-            ),
-            Self::XChaCha20Poly1305 {
-                key,
-                nonce,
-                aad,
-                msg,
-            } => aead_encrypt(
-                XChaCha20Poly1305::new_from_slice(&*key)
-                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
-                &*nonce,
-                aad,
-                msg,
-            ),
-        }
-    }
+pub fn chacha20poly1305_encrypt(
+    key: [u8; SIZE_32],
+    nonce: [u8; SIZE_12],
+    aad: &[u8],
+    cipher: &[u8],
+) -> Result<Vec<u8>> {
+    let aead = ChaCha20Poly1305::new_from_slice(&key)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+    aead_decrypt(aead, &nonce, aad, cipher)
+}
 
-    /// Decrypt ...
-    pub fn decrypt(self) -> Result<Vec<u8>> {
-        match self {
-            Self::ChaCha20Poly1305 {
-                key,
-                nonce,
-                aad,
-                msg,
-            } => aead_decrypt(
-                ChaCha20Poly1305::new_from_slice(&*key)
-                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
-                &*nonce,
-                aad,
-                msg,
-            ),
-            Self::XChaCha20Poly1305 {
-                key,
-                nonce,
-                aad,
-                msg,
-            } => aead_decrypt(
-                XChaCha20Poly1305::new_from_slice(&*key)
-                    .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
-                &*nonce,
-                aad,
-                msg,
-            ),
-        }
-    }
+pub fn xchacha20poly1305_decrypt(
+    key: [u8; SIZE_32],
+    nonce: [u8; SIZE_24],
+    aad: &[u8],
+    plain: &[u8],
+) -> Result<Vec<u8>> {
+    let aead = XChaCha20Poly1305::new_from_slice(&key)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+    aead_encrypt(aead, &nonce, aad, plain)
+}
+
+pub fn xchacha20poly1305_encrypt(
+    key: [u8; SIZE_32],
+    nonce: [u8; SIZE_24],
+    aad: &[u8],
+    cipher: &[u8],
+) -> Result<Vec<u8>> {
+    let aead = XChaCha20Poly1305::new_from_slice(&key)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+    aead_decrypt(aead, &nonce, aad, cipher)
 }
 
 fn aead_decrypt(aead: impl Aead, nonce: &[u8], aad: &[u8], cipher: &[u8]) -> Result<Vec<u8>> {
@@ -110,3 +76,82 @@ fn aead_encrypt(aead: impl Aead, nonce: &[u8], aad: &[u8], plain: &[u8]) -> Resu
 
     Ok(cipher)
 }
+
+// ______________________________
+// /// Symmetric ...
+// pub enum Symmetric<'a> {
+//     ChaCha20Poly1305 {
+//         key: Box<[u8; SIZE_32]>,
+//         nonce: Box<[u8; SIZE_12]>,
+//         aad: &'a [u8],
+//         msg: &'a [u8],
+//     },
+//     XChaCha20Poly1305 {
+//         key: Box<[u8; SIZE_32]>,
+//         nonce: Box<[u8; SIZE_24]>,
+//         aad: &'a [u8],
+//         msg: &'a [u8],
+//     },
+// }
+
+// impl Symmetric<'_> {
+//     /// Encrypt ...
+//     pub fn encrypt(self) -> Result<Vec<u8>> {
+//         match self {
+//             Self::ChaCha20Poly1305 {
+//                 key,
+//                 nonce,
+//                 aad,
+//                 msg,
+//             } => aead_encrypt(
+//                 ChaCha20Poly1305::new_from_slice(&*key)
+//                     .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+//                 &*nonce,
+//                 aad,
+//                 msg,
+//             ),
+//             Self::XChaCha20Poly1305 {
+//                 key,
+//                 nonce,
+//                 aad,
+//                 msg,
+//             } => aead_encrypt(
+//                 XChaCha20Poly1305::new_from_slice(&*key)
+//                     .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+//                 &*nonce,
+//                 aad,
+//                 msg,
+//             ),
+//         }
+//     }
+
+//     /// Decrypt ...
+//     pub fn decrypt(self) -> Result<Vec<u8>> {
+//         match self {
+//             Self::ChaCha20Poly1305 {
+//                 key,
+//                 nonce,
+//                 aad,
+//                 msg,
+//             } => aead_decrypt(
+//                 ChaCha20Poly1305::new_from_slice(&*key)
+//                     .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+//                 &*nonce,
+//                 aad,
+//                 msg,
+//             ),
+//             Self::XChaCha20Poly1305 {
+//                 key,
+//                 nonce,
+//                 aad,
+//                 msg,
+//             } => aead_decrypt(
+//                 XChaCha20Poly1305::new_from_slice(&*key)
+//                     .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?,
+//                 &*nonce,
+//                 aad,
+//                 msg,
+//             ),
+//         }
+//     }
+// }
