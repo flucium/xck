71e059273c99c885414dce7f491e6887f44b9bd1
flucium <14249224+flucium@users.noreply.github.com>
2023-06-26 12:19:03 +0900

    clean ./src/asymmetric.rs

--- src/asymmetric.rs
+++ src/asymmetric.rs
@@ -3,43 +3,39 @@
 // use ed25519_dalek::{Signer as SignerDelek, Verifier as VerifierDelek};
 
 /*
-    -Signer
-    RSA SSA PSS
-    // RSA SSA PKCS#1 v1.5
+    Sign/Verify
+    RSASSA-PSS-1024-SHA256
+    RSASSA-PSS-2048-SHA256
+    RSASSA-PSS-4096-SHA256
 
-    -Verifier
-    RSA SSA PSS
-    // RSA SSA PKCS#1 v1.5
+    RSASSA-PSS-1024-SHA386
+    RSASSA-PSS-2048-SHA386
+    RSASSA-PSS-4096-SHA386
+
+    RSASSA-PSS-1024-SHA512
+    RSASSA-PSS-2048-SHA512
+    RSASSA-PSS-4096-SHA512
+
+    Encrypt/Decrypt
+    RSA-OAEP-1024-SHA256
+    RSA-OAEP-2048-SHA256
+    RSA-OAEP-4096-SHA256
+
+    RSA-OAEP-1024-SHA384
+    RSA-OAEP-2048-SHA384
+    RSA-OAEP-4096-SHA384
+
+    RSA-OAEP-1024-SHA512
+    RSA-OAEP-2048-SHA512
+    RSA-OAEP-4096-SHA512
 */
 
 /*
-    -Encryptor
-    RSA OAEP
+    Sign/Verify
+    Ed25519
 */
 
 /*
-    -Decryptor
-    RSA OAEP
+    Key Exchange
+    X25519
 */
-
-/*
-    -Hash
-    Sha1
-    Sha256
-    Sha384
-    Sha512
-    Sha512/256
-*/
-
-
-// pub enum RsaPaddingScheme {
-//     OaepSha256,
-//     OaepSha384,
-//     OaepSha512,
-// }
-
-// pub enum RsaSignatureScheme {
-//     PssSha256,
-//     PssSha384,
-//     PssSha512,
-// }
