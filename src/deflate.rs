23e324b7175cb8d4f0f93dbc66d15da36781546b
flucium <14249224+flucium@users.noreply.github.com>
2023-07-02 14:24:42 +0900

    update ./src/deflate.rs

--- src/deflate.rs
+++ src/deflate.rs
@@ -1,28 +1,122 @@
 use crate::{Error, ErrorKind, Result};
-use flate2::Compression;
+
+use flate2::{
+    write::{DeflateDecoder, DeflateEncoder, GzDecoder, GzEncoder, ZlibDecoder, ZlibEncoder},
+    Compression,
+};
+
 use std::io::Write;
 
-pub fn compress(bytes: &[u8]) -> Result<Vec<u8>> {
-    let mut encoder = flate2::write::DeflateEncoder::new(Vec::new(), Compression::default());
+// Compression::default()
+pub const DEFAULT_DEVEL: u32 = 6;
+
+/// Gz Compress ...
+pub fn gz_compress(level: u32, bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::new(level));
+
     encoder
-        .write(bytes)
+        .write_all(bytes)
         .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
-    let bytes = encoder
+
+    encoder
         .finish()
-        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
-    Ok(bytes)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))
 }
 
-pub fn decompress(bytes: &[u8]) -> Result<Vec<u8>> {
-    let mut decoder = flate2::write::DeflateDecoder::new(Vec::new());
+/// Gz Decompress ...
+pub fn gz_decompress(bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut decoder = GzDecoder::new(Vec::new());
+
     decoder
-        .write(bytes)
+        .write_all(bytes)
         .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
-    let bytes = decoder
+
+    decoder
         .finish()
-        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
-    Ok(bytes)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))
 }
+
+/// Zlib Compress ...
+pub fn zlib_compress(level: u32, bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::new(level));
+
+    encoder
+        .write_all(bytes)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+
+    encoder
+        .finish()
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))
+}
+
+/// Zlib Decompress ...
+pub fn zlib_decompress(bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut decoder = ZlibDecoder::new(Vec::new());
+
+    decoder
+        .write_all(bytes)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+
+    decoder
+        .finish()
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))
+}
+
+/// Deflate Compress ...
+pub fn defalte_compress(level: u32, bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut encoder = DeflateEncoder::new(Vec::new(), Compression::new(level));
+
+    encoder
+        .write_all(bytes)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+
+    encoder
+        .finish()
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))
+}
+
+/// Deflate Decompress ...
+pub fn deflate_decompress(bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut decoder = DeflateDecoder::new(Vec::new());
+
+    decoder
+        .write_all(bytes)
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+
+    decoder
+        .finish()
+        .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))
+}
+
+// pub fn compress(level: u32, bytes: &[u8]) -> Result<Vec<u8>> {
+//     defalte_compress(level, bytes)
+// }
+
+// pub fn decompress(bytes: &[u8]) -> Result<Vec<u8>> {
+//     deflate_decompress(bytes)
+// }
+
+// pub fn compress(bytes: &[u8]) -> Result<Vec<u8>> {
+//     let mut encoder = flate2::write::DeflateEncoder::new(Vec::new(), Compression::default());
+//     encoder
+//         .write(bytes)
+//         .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+//     let bytes = encoder
+//         .finish()
+//         .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+//     Ok(bytes)
+// }
+
+// pub fn decompress(bytes: &[u8]) -> Result<Vec<u8>> {
+//     let mut decoder = flate2::write::DeflateDecoder::new(Vec::new());
+//     decoder
+//         .write(bytes)
+//         .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+//     let bytes = decoder
+//         .finish()
+//         .map_err(|err| Error::new(ErrorKind::Todo, err.to_string()))?;
+//     Ok(bytes)
+// }
 
 // Deflate compress from io::Read
 // pub fn compress_from_read<R>(r: R) -> Result<Vec<u8>>
