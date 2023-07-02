5cc52789ecfa3e2f35fe91d29d71881b274e127c
flucium <14249224+flucium@users.noreply.github.com>
2023-07-02 15:53:36 +0900

    refactor ./src/size.rs

--- src/size.rs
+++ src/size.rs
@@ -1,20 +1,14 @@
-/// 12-byte
-pub const SIZE_12: usize = 12;
-
-/// 16-byte
-pub const SIZE_16: usize = 16;
-
-// 20-byte
-// pub const SIZE_20: usize = 20;
-
-/// 24-byte
-pub const SIZE_24: usize = 24;
+/// 64-byte
+pub const SIZE_64: usize = 64;
 
 /// 32-byte
 pub const SIZE_32: usize = 32;
 
-// 34-byte
-// pub const SIZE_34: usize = 34;
+/// 24-byte
+pub const SIZE_24: usize = 24;
 
-/// 64-byte
-pub const SIZE_64: usize = 64;
+/// 16-byte
+pub const SIZE_16: usize = 16;
+
+/// 12-byte
+pub const SIZE_12: usize = 12;
