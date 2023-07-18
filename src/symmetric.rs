117cab5d239280f4c967b81de45f523ba568cba1
flucium <14249224+flucium@users.noreply.github.com>
2023-07-18 12:58:04 +0900

    update ./src/symmetric.rs

--- src/symmetric.rs
+++ src/symmetric.rs
@@ -12,57 +12,57 @@ use aes_gcm::{Aes128Gcm, Aes256Gcm};
 type Aes192Gcm = aes_gcm::AesGcm<aes_gcm::aes::Aes192, aead::consts::U12>;
 
 pub fn aes_256_gcm_decrypt(
-    key: [u8; SIZE_32],
+    key: &[u8; SIZE_32],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     cipher: &[u8],
 ) -> Result<Vec<u8>> {
-    aead_decrypt(Aes256Gcm::new_from_slice(&key).unwrap(), nonce, aad, cipher)
+    aead_decrypt(Aes256Gcm::new_from_slice(key).unwrap(), nonce, aad, cipher)
 }
 
 pub fn aes_256_gcm_encrypt(
-    key: [u8; SIZE_32],
+    key: &[u8; SIZE_32],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     plain: &[u8],
 ) -> Result<Vec<u8>> {
-    aead_encrypt(Aes256Gcm::new_from_slice(&key).unwrap(), nonce, aad, plain)
+    aead_encrypt(Aes256Gcm::new_from_slice(key).unwrap(), nonce, aad, plain)
 }
 
 pub fn aes_192_gcm_decrypt(
-    key: [u8; SIZE_24],
+    key: &[u8; SIZE_24],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     cipher: &[u8],
 ) -> Result<Vec<u8>> {
-    aead_decrypt(Aes192Gcm::new_from_slice(&key).unwrap(), nonce, aad, cipher)
+    aead_decrypt(Aes192Gcm::new_from_slice(key).unwrap(), nonce, aad, cipher)
 }
 
 pub fn aes_192_gcm_encrypt(
-    key: [u8; SIZE_24],
+    key: &[u8; SIZE_24],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     plain: &[u8],
 ) -> Result<Vec<u8>> {
-    aead_encrypt(Aes192Gcm::new_from_slice(&key).unwrap(), nonce, aad, plain)
+    aead_encrypt(Aes192Gcm::new_from_slice(key).unwrap(), nonce, aad, plain)
 }
 
 pub fn aes_128_gcm_decrypt(
-    key: [u8; SIZE_16],
+    key: &[u8; SIZE_16],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     cipher: &[u8],
 ) -> Result<Vec<u8>> {
-    aead_decrypt(Aes128Gcm::new_from_slice(&key).unwrap(), nonce, aad, cipher)
+    aead_decrypt(Aes128Gcm::new_from_slice(key).unwrap(), nonce, aad, cipher)
 }
 
 pub fn aes_128_gcm_encrypt(
-    key: [u8; SIZE_16],
+    key: &[u8; SIZE_16],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     plain: &[u8],
 ) -> Result<Vec<u8>> {
-    aead_encrypt(Aes128Gcm::new_from_slice(&key).unwrap(), nonce, aad, plain)
+    aead_encrypt(Aes128Gcm::new_from_slice(key).unwrap(), nonce, aad, plain)
 }
 
 /*
@@ -73,13 +73,13 @@ pub fn aes_128_gcm_encrypt(
 */
 
 pub fn xchacha20_poly1305_decrypt(
-    key: [u8; SIZE_32],
+    key: &[u8; SIZE_32],
     nonce: &[u8; SIZE_24],
     aad: &[u8],
     cipher: &[u8],
 ) -> Result<Vec<u8>> {
     aead_decrypt(
-        XChaCha20Poly1305::new_from_slice(&key).unwrap(),
+        XChaCha20Poly1305::new_from_slice(key).unwrap(),
         nonce,
         aad,
         cipher,
@@ -87,13 +87,13 @@ pub fn xchacha20_poly1305_decrypt(
 }
 
 pub fn xchacha20_poly1305_encrypt(
-    key: [u8; SIZE_32],
+    key: &[u8; SIZE_32],
     nonce: &[u8; SIZE_24],
     aad: &[u8],
     plain: &[u8],
 ) -> Result<Vec<u8>> {
     aead_encrypt(
-        XChaCha20Poly1305::new_from_slice(&key).unwrap(),
+        XChaCha20Poly1305::new_from_slice(key).unwrap(),
         nonce,
         aad,
         plain,
@@ -101,13 +101,13 @@ pub fn xchacha20_poly1305_encrypt(
 }
 
 pub fn chacha20_poly1305_decrypt(
-    key: [u8; SIZE_32],
+    key: &[u8; SIZE_32],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     cipher: &[u8],
 ) -> Result<Vec<u8>> {
     aead_decrypt(
-        ChaCha20Poly1305::new_from_slice(&key).unwrap(),
+        ChaCha20Poly1305::new_from_slice(key).unwrap(),
         nonce,
         aad,
         cipher,
@@ -115,13 +115,13 @@ pub fn chacha20_poly1305_decrypt(
 }
 
 pub fn chacha20_poly1305_encrypt(
-    key: [u8; SIZE_32],
+    key: &[u8; SIZE_32],
     nonce: &[u8; SIZE_12],
     aad: &[u8],
     plain: &[u8],
 ) -> Result<Vec<u8>> {
     aead_encrypt(
-        ChaCha20Poly1305::new_from_slice(&key).unwrap(),
+        ChaCha20Poly1305::new_from_slice(key).unwrap(),
         nonce,
         aad,
         plain,
