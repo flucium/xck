d4bd08f5422620fae6e7673818b8ca38254e7526
flucium <14249224+flucium@users.noreply.github.com>
2023-07-20 15:35:02 +0900

    update ./src/format.rs
    to/from to encode/decode

--- tests/format.rs
+++ tests/format.rs
@@ -1,37 +1,37 @@
 #[test]
-fn to_base64() {
+fn base64_encode() {
     const TEST_B64_STRING: &str = "aGVsbG8=";
 
     // TEST_BYTES is hello
     const TEST_BYTES: [u8; 5] = [104, 101, 108, 108, 111];
 
     assert_eq!(
-        xck::format::to_base64(&TEST_BYTES).unwrap(),
+        xck::format::base64_encode(&TEST_BYTES).unwrap(),
         TEST_B64_STRING
     );
 }
 
 #[test]
-fn from_base64() {
+fn base64_decode() {
     const TEST_BYTES: [u8; 5] = [104, 101, 108, 108, 111];
 
     const TEST_B64_STRING: &str = "aGVsbG8=";
 
     assert_eq!(
-        xck::format::from_base64(&TEST_B64_STRING).unwrap(),
+        xck::format::base64_decode(&TEST_B64_STRING).unwrap(),
         TEST_BYTES
     );
 }
 
 #[test]
-fn to_hex() {
+fn hex_encode() {
     // TEST_BYTES is hello
     const TEST_BYTES: [u8; 5] = [104, 101, 108, 108, 111];
     
     const TEST_HEX_STRING:&str="68656c6c6f";
 
     assert_eq!(
-        xck::format::to_hex(&TEST_BYTES),
+        xck::format::hex_encode(&TEST_BYTES),
         TEST_HEX_STRING
     );
 }
